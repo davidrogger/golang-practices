f6f43a857240a49c4b70676ebdf59cd893acf6b1
davidrogger <davidrogger.a@gmail.com>
2023-04-11 13:31:48 -0300

    FEAT: exericse 03-06

---
+++ Exercises/03-exercises/exercise03-06.go
@@ -0,0 +1,14 @@
+// Crie um programa que demonstre o funcionamento da declaração if
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 5
+	if x > 1 {
+		fmt.Println("Maior que 1")
+	} else {
+		fmt.Println("Menor que 1")
+	}
+}
