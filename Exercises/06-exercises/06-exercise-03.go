afef91963824512df52c01bcf47d86da6fe3ad61
davidrogger <davidrogger.a@gmail.com>
2023-04-18 17:03:45 -0300

    FEAT: exercise 06-03

---
+++ Exercises/06-exercises/06-exercise-03.go
@@ -0,0 +1,13 @@
+/*
+	- Utilize a declaração defer de maneira que demonstre que sua execução
+	só ocorre ao final do contexto ao qual ela pertence.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("Seria o primeiro sem o defer")
+	fmt.Println("Segundo")
+}
